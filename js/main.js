91b6f13fc10c5a83ba1662db10fa129c9d36f3b5
BeEn01t <been01t@BeEn01tonnoMacBook-Air.local>
2023-08-06 20:38:48 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+window.addEventListener("DOMContentLoaded", function () {
+  const alertBtn = document.getElementById("alert-btn");
+  alertBtn.addEventListener("click", function (e) {
+    window.alert("これはアラートです。");
+  });
+});
+
+// let count = 0;
+// var change = function(){
+//   if(count % 3 == 0){
+//     
+//   }
+//   else if(count % 3 == 1){
+//     
+//   }
+//   else {
+//     
+//   }
+//   count = count % 3;
+//   count++;
+// };
+
+// setInterval(change,500);
